4b33f65b5cde556859f650ee60fb47d6cd5dac60
Felix Ouma <fatonoh@gmail.com>
2019-01-05 15:54:01 +0300

    [finshes #162952679] finished designin landing page after adding menu for mobile navigation

---
+++ UI/static/js/main.js
@@ -0,0 +1,10 @@
+
+function menuDrawer(){
+	var userMenu = document.getElementById('mobile-menu')
+	if(userMenu.style.display){
+			userMenu.style.display = 'none';
+	}else{
+			userMenu.style.display = 'block';
+	}
+}
+
