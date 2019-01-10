bc0b4f9a2de90c57820d32b15d11d24fbe3e54c0
Felix Ouma <fatonoh@gmail.com>
2019-01-10 21:08:07 +0300

    [bug #163118741] fixed mixed indents and spaces

--- app/api/v1/utils/user_validator.py
+++ app/api/v1/utils/user_validator.py
@@ -1,50 +1,49 @@
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 import re
+
 
 class UsersHelper:
 	def __init__(self):
 		''' Contains helper methods for UsersModel class '''
 		pass
 		
-	def is_valid_user_request(self, keys_expected,user_request):
+	def is_valid_user_request(self, keys_expected, user_request):
 		if len(user_request.get_json().keys()) == 0:
-			return 0 , jsonify({'message':'sent an empty request'})
+			return 0, jsonify({'message': 'sent an empty request'})
 			
 		sent_keys = user_request.get_json().keys()
 		
 		for key in keys_expected:
 			if key not in sent_keys:
-				return 0 , jsonify({'message':'missing required field {} from request'.format(key)})
-	
-		return 1	, jsonify({'message':'user request was valid'})
+				return 0, jsonify({'message': 'missing required field {} from request'.format(key)})
+
+		return 1	, jsonify({'message': 'user request was valid'})
 		
-	def is_blank_field(self,user_request):
+	def is_blank_field(self, user_request):
 		
-		for key , user_item in user_request.get_json().items():
+		for key, user_item in user_request.get_json().items():
 			if user_item == '':
-				return False , jsonify({'message':'Field value was empty: {}'.format(key)})
+				return False, jsonify({'message': 'Field value was empty: {}'.format(key)})
 				
-		return True , jsonify({'message':''})
+		return True, jsonify({'message': ''})
 		
-	def is_available(self , value , key , users , user_id=0):
+	def is_available(self, value, key, users, user_id=0):
 		for user in users:
 			if user[key] == value and user_id != user['user_id']:
-				return False , jsonify({'message':'{} field value is not available: '.format(key)})
+				return False, jsonify({'message': '{} field value is not available: '.format(key)})
 		
-		return True , jsonify({'message':''})
+		return True, jsonify({'message': ''})
 		
-	def is_valid_email(self,email):
-		if re.match("\A(?P<name>[\w\-_]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z",email,re.IGNORECASE) and len(email) > 7:
-			return True , jsonify({'message':''})
-		return False , jsonify({'message':'email is not valid:'})
-		
-		
-	def is_valid_password(self,password,cpassword):
+	def is_valid_email(self, email):
+		if re.match("\A(?P<name>[\w\-_]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z", email, re.IGNORECASE) and len(email) > 7:
+			return True, jsonify({'message': ''})
+		return False, jsonify({'message': 'email is not valid:'})
+			
+	def is_valid_password(self, password, cpassword):
 		if password == cpassword:
 			if re.match(r"^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,12}$", password):
-				return True , jsonify({'message':''})
-			return False , jsonify({'message':'Password should have at least 5 characters, a lowercase, an uppercase and a special character e.g Felix45@ or Emily16#'})
+				return True, jsonify({'message': ''})
+			return False, jsonify({'message': 'Password should have at least 5 characters, a lowercase, an uppercase and a special character e.g Felix45@ or Emily16#'})
 		else:
-			return False , jsonify({'message':'Confirm password provided did not match password'})
-		
-		
+			return False, jsonify({'message': 'Confirm password provided did not match password'})
+			
--- app/api/v1/views/meetups_view.py
+++ app/api/v1/views/meetups_view.py
@@ -1,18 +1,21 @@
-from flask import Blueprint, Flask , jsonify , request 
+from flask import Blueprint, Flask, jsonify, request
 
 from app.api.v1.models.meetups_model import MeetUpsModel
 
-meetupV1 = Blueprint('meetup_v1',__name__,url_prefix='/api/v1')
+meetupV1 = Blueprint('meetup_v1', __name__, url_prefix='/api/v1')
 
 meetups_model = MeetUpsModel()
+
 
 @meetupV1.route('/meetups', methods=['POST'])
 def add_meetup():
 	return meetups_model.add_meetup(request)
 
+
 @meetupV1.route('/meetups/upcoming', methods=['GET'])
 def get_meetup():
 	return meetups_model.get_meetups()
+
 
 @meetupV1.route('/meetups/<int:meetup_id>', methods=['GET'])
 def get_a_meetup(meetup_id):
