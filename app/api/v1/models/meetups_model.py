f1d5e6aea0fee4505d09b73dfcdfb85187af1ee4
Felix Ouma <fatonoh@gmail.com>
2019-01-10 08:24:25 +0300

    [feature  #163046496] Added validations to create meetup endpoint

--- app/api/v1/models/meetups_model.py
+++ app/api/v1/models/meetups_model.py
@@ -1,16 +1,29 @@
 """ Manages all the meetup functions """
 from datetime import date
 from flask import Flask, request, jsonify
+from app.api.v1.utils.user_validator import UsersHelper
 
 class MeetUpsModel():
 
     def __init__(self):
         self.meetups = []
+        self.helpers = UsersHelper()
 
     def add_meetup(self,user_request):
         user = {}
 
-        
+        keys_expected = ["location","images","topic","happeningOn","Tags"]
+
+        ret = self.helpers.is_valid_user_request(keys_expected,user_request)
+
+        if ret[0] == 0:
+            return ret[1]  , 400
+
+
+        ret = self.helpers.is_blank_field(user_request)
+
+        if ret[0] == 0:
+            return ret[1] , 400
 
         user["id"] = len(self.meetups) + 1
         user["created_on"]  = date.today()
