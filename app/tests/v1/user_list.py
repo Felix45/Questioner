2c3a0d47b8390a674efe121a8c617dabbb69a37e
Felix Ouma <fatonoh@gmail.com>
2019-01-10 21:15:31 +0300

    [finishes #163118741] fixed mixed indents and spaces

--- app/tests/v1/user_list.py
+++ app/tests/v1/user_list.py
@@ -2,101 +2,96 @@ user_1 = {}
 
 user_2 = {
     "firstname": "",
-	"lastname" : "",
+    "lastname": "",
     "othername": "",
-    "email"    : "fatonoh@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "fatonoh",
-    "isAdmin"  : "",
+    "email": "fatonoh@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "fatonoh",
+    "isAdmin": "",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_3 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "fatonoh@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "fatonoh",
-    "isAdmin"  : "False",
+    "email": "fatonoh@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "fatonoh",
+    "isAdmin": "False",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_4 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "atonoh@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "fatonoh",
-    "isAdmin"  : "False",
+    "email": "atonoh@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "fatonoh",
+    "isAdmin": "False",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_5 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "fatonoh@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "fatonoh",
-    "isAdmin"  : "True",
+    "email": "fatonoh@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "fatonoh",
+    "isAdmin": "True",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_8 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "fookinyoyahoo.com",
-    "phoneNumber" : "0724603328",
-    "username" : "Felix45",
-    "isAdmin"  : "False",
+    "email": "fookinyoyahoo.com",
+    "phoneNumber": "0724603328",
+    "username": "Felix45",
+    "isAdmin": "False",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_9 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "fratonoh@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "fatonoh",
-    "isAdmin"  : "False",
+    "email": "fratonoh@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "fatonoh",
+    "isAdmin": "False",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_10 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "kimondio@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "Kimondi",
-    "isAdmin"  : "True",
+    "email": "kimondio@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "Kimondi",
+    "isAdmin": "True",
     "cpassword": "felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
 
 user_11 = {
     "firstname": "Felix",
-	"lastname" : "Ouma",
+    "lastname": "Ouma",
     "othername": "Okish",
-    "email"    : "fratonoh@gmail.com",
-    "phoneNumber" : "0724603328",
-    "username" : "fello",
-    "isAdmin"  : "False",
+    "email": "fratonoh@gmail.com",
+    "phoneNumber": "0724603328",
+    "username": "fello",
+    "isAdmin": "False",
     "cpassword": "Felix45@",
-    "password" : "Felix45@"
+    "password": "Felix45@"
 }
-
-
-
-
-
--- run.py
+++ run.py
@@ -5,9 +5,3 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
