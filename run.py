45c5291da837b64068e862ff6394b016465a76cd
Felix Ouma <fatonoh@gmail.com>
2019-01-13 22:15:48 +0300

    [chore #163177932] updated run.py file

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 """ run the flask application """
-import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
