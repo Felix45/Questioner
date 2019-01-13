2eff77ee0bef85bbcaf39ee3d54b9a741e7391a9
Felix Ouma <fatonoh@gmail.com>
2019-01-13 20:49:49 +0300

    [chore #163177932] Added port number to run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 """ run the flask application """
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
