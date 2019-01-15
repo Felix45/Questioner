ace7b2bb991e79f0c741ce04df6d14c47a9effc5
Felix Ouma <fatonoh@gmail.com>
2019-01-15 19:44:18 +0300

    [bug #163228935] Removed logic error from test_meetups

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -180,27 +180,31 @@ class MeetupTest(SetUpTestClient):
                                content_type='application/json')
         self.assertEqual(res.status_code, 404)
 
-        def test_get_a_question(self):
-                question = {
-                        "user": 1,
-                        "meetup": 1,
-                        "title": "How do i install python in Ubuntu 18.04?",
-                        "body": "I have been trying to install python in ubuntu\
+    def test_get_a_question(self):
+        """ Allows a user to fetch a specific question """
+        question = {
+                "user": 1,
+                "meetup": 1,
+                "title": "How do i install python in Ubuntu 18.04?",
+                "body": "I have been trying to install python in ubuntu\
                                         18.04 with no success, someone help please."
-                }
-                question_two = {
-                        "user": 1,
-                        "meetup": 1,
-                        "title": "How do i install python in Ubuntu 18.04?",
-                        "body": "I have been trying to install python in ubuntu\
-                                        18.04 with no success, someone help please."
-                }
-                res = self.client.post("/api/v1/questions", json=question,
+        }
+        question_two = {
+                "user": 1,
+                "meetup": 1,
+                "title": "How do i install python in Ubuntu 18.04?",
+                "body": "I have been trying to install python in ubuntu\
+                                18.04 with no success, someone help please."
+        }
+        res = self.client.post("/api/v1/questions", json=question,
                                content_type='application/json')
-                self.assertEqual(res.status_code, 201)
-                res = self.client.post("/api/v1/questions", json=question_two,
+        self.assertEqual(res.status_code, 201)
+        res = self.client.post("/api/v1/questions", json=question_two,
                                content_type='application/json')
-                self.assertEqual(res.status_code, 201)
-
-            
-
+        self.assertEqual(res.status_code, 201)
+        res = self.client.get("/api/v1/questions/1")
+        self.assertEqual(res.status_code, 200)
+        res = self.client.get("/api/v1/questions/2")
+        self.assertEqual(res.status_code, 200)
+        res = self.client.get("/api/v1/questions/100")
+        self.assertEqual(res.status_code, 404)
