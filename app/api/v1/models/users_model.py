cf143fcc29d0cd38ca0d4e02f9c8f1b35acee511
Felix Ouma <fatonoh@gmail.com>
2019-01-15 11:15:38 +0300

    [feature #163046463] added method to allow tests pass

--- app/api/v1/models/users_model.py
+++ app/api/v1/models/users_model.py
@@ -1,7 +1,9 @@
 """ Manages users details """
 from datetime import date
 from flask import Flask, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.api.v1.utils.user_validator import UsersHelper
+
 
 users = []
 helpers = UsersHelper()
@@ -47,14 +49,14 @@ class UsersModel:
 
     def insert_db(self, user_request):
         new_user = {}
-
+        password = user_request.get_json()['password']
         new_user['user_id'] = len(users)+1
         new_user['firstname'] = user_request.get_json()['firstname']
         new_user['lastname'] = user_request.get_json()['lastname']
         new_user['othername'] = user_request.get_json()['othername']
         new_user['username'] = user_request.get_json()['username']
         new_user['email'] = user_request.get_json()['email']
-        new_user['password'] = user_request.get_json()['password']
+        new_user['password'] = generate_password_hash(password)
         new_user['registered'] = date.today()
         new_user['isAdmin'] = user_request.get_json()['isAdmin']
         new_user['phoneNumber'] = user_request.get_json()['phoneNumber']
@@ -68,3 +70,27 @@ class UsersModel:
         if len(users) == 0:
             return jsonify({'msg': 'no user was found'})    
         return jsonify({"data": users}), 200
+
+    def login_user(self, request):
+        global users
+        keys_expected = ['username', 'password']
+
+        validator = helpers.is_valid_user_request(keys_expected, request)
+
+        if not validator[0]:
+            return validator[1], 400
+
+        validator = helpers.is_blank_field(request)
+
+        if not validator[0]:
+            return validator[1], 400
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        logged_in = [u for u in users if u['username'] == username and 
+                     check_password_hash(u['password'], password)]
+
+        if len(logged_in) > 0:
+            return jsonify({'msg': 'logged in', "data": logged_in}), 200
+        return jsonify({'msg': 'user {} not found:'.format(username)}), 404
