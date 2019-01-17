84734230884ea5eaf3344903d7d452d6bd4f254c
Felix Ouma <fatonoh@gmail.com>
2019-01-17 14:15:23 +0300

    [start #163281826] Added meetups application blue print

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 """ Register application Blueprints """
 from flask import Flask
 from app.api.v2.views.user_view import userV2
-#from app.api.v1.views.meetups_view import meetupV1
+from app.api.v2.views.meetups_view import meetupV2
 
 
 def create_app():
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Okish45@@'
     app.register_blueprint(userV2, url_prefix='/api/v2')
-    #app.register_blueprint(meetupV1, url_prefix='/api/v1')
+    app.register_blueprint(meetupV2, url_prefix='/api/v2')
 
     @app.route("/")
     def function():
