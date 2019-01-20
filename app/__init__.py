85639b262757fe6225f9ee2e319519303fe18b57
Felix Ouma <fatonoh@gmail.com>
2019-01-20 07:25:18 +0300

    [start #163346435] Added questions blueprint to the application

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from app.api.v2.views.user_view import userV2
 from app.api.v2.views.meetups_view import meetupV2
+from app.api.v2.views.questions_view import questionV2
+
 
 
 def create_app():
@@ -10,6 +12,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'Okish45@@'
     app.register_blueprint(userV2, url_prefix='/api/v2')
     app.register_blueprint(meetupV2, url_prefix='/api/v2')
+    app.register_blueprint(questionV2, url_prefix='/api/v2')
 
     @app.route("/")
     def function():
