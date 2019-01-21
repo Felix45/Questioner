33b5ef35d2b61efbef5ce499404368e8dbfc7de4
Felix Ouma <fatonoh@gmail.com>
2019-01-21 08:14:48 +0300

    [bug #163348045] updated the parent test class, to set up test database

--- app/tests/v2/basetest.py
+++ app/tests/v2/basetest.py
@@ -1,18 +1,21 @@
 ''' Sets up the test client '''
 import unittest
 from app import create_app
+from app.dbconnection import DbConnection
 
 
 class SetUpTestClient(unittest.TestCase):
 	def setUp(self):
 		''' Sets up the test client for stackoverflowlite Api '''
-		self.app = create_app()
+		self.app = create_app('testing')
 		self.client = self.app.test_client()
+		with self.app.app_context():
+			self.db = DbConnection().setUpTestDb()
 		
 	def tear_down(self):
 		''' Destroys the test client '''
-		self.app.testing = False
-		self.app = None
+		with self.app.app_context():
+			DbConnection().destroyTestDb()
 		
 if __name__ == '__main__':
 	unittest.main(verbosity=1)
