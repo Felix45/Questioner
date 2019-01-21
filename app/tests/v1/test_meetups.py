6bf0933db44e6b524869bbc09a520e114aa1b00d
Felix Ouma <fatonoh@gmail.com>
2019-01-21 08:09:44 +0300

    [bug #163348045] Refactored tests related to questions and added pep8 standards

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -24,11 +24,13 @@ class MeetupTest(SetUpTestClient):
                     "Tags": ["women", "mums", "ladies"]
         }
 
-        res = self.client.post("/api/v1/meetups", json=meetup, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup,
+                               content_type='application/json')
         self.assertEqual(res.status_code, 200)
         self.assertIn('user was added', str(res.data))
 
-        res = self.client.post("/api/v1/meetups", json=meetup_two, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup_two,
+                               content_type='application/json')
         self.assertEqual(res.status_code, 400)
         self.assertIn('Field value was empty:', str(res.data))
 
@@ -62,11 +64,14 @@ class MeetupTest(SetUpTestClient):
                 "happeningOn": datetime.datetime(2019, 5, 17),
                 "Tags": ["women", "mums", "ladies"]
         }
-        res = self.client.post("/api/v1/meetups", json=meetup, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup,
+                               content_type='application/json')
         self.assertEqual(res.status_code, 200)
-        res = self.client.post("/api/v1/meetups", json=meetup_one, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup_one,
+                               content_type='application/json')
         self.assertEqual(res.status_code, 400)
-        res = self.client.post("/api/v1/meetups", json=meetup_two, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup_two, 
+                               content_type='application/json')
         self.assertEqual(res.status_code, 200)
 
         res = self.client.get("/api/v1/meetups/upcoming")
@@ -89,9 +94,11 @@ class MeetupTest(SetUpTestClient):
                 "happeningOn": datetime.datetime(2019, 5, 17),
                 "Tags": ["law", "medicine", "Education"]
         }
-        res = self.client.post("/api/v1/meetups", json=meetup, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup,
+                               content_type='application/json')
         self.assertEqual(res.status_code, 200)
-        res = self.client.post("/api/v1/meetups", json=meetup_one, content_type='application/json')
+        res = self.client.post("/api/v1/meetups", json=meetup_one,
+                               content_type='application/json')
         self.assertEqual(res.status_code, 200)
 
         res = self.client.get("/api/v1/meetups/1")
@@ -103,42 +110,7 @@ class MeetupTest(SetUpTestClient):
         res = self.client.get("/api/v1/meetups/50")
         self.assertEqual(res.status_code, 404)
         self.assertIn('user was not found', str(res.data))
-
-    def test_add_questions(self):
-        question = {
-
-            "user": 1,
-            "meetup": 1,
-            "title": "How do i install python in Ubuntu 18.04?",
-            "body": "I have been trying to install python in ubuntu 18.04 with no success, someone help please."
-        }
-        res = self.client.post("/api/v1/questions", json=question,
-                               content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        self.assertIn('question was added', str(res.data))
-
-    def test_vote_question(self):
-        """ Allows user to upVote or downVote on a question """
-        question = {
-            "user": 1,
-            "meetup": 1,
-            "title": "How do i install python in Ubuntu 18.04?",
-            "body": "I have been trying to install python in ubuntu\
-                             18.04 with no success, someone help please."
-        }
-        res = self.client.post("/api/v1/questions", json=question,
-                               content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        res = self.client.patch("/api/v1/questions/1/upvote", json=question,
-                                content_type="application/json")
-        self.assertEqual(res.status_code, 201)
-        self.assertIn("upvote was successful", str(res.data))
-
-        res = self.client.patch("/api/v1/questions/1/downvote", json=question,
-                                content_type="application/json")
-        self.assertEqual(res.status_code, 201)
-        self.assertIn("downvote was successful", str(res.data))
- 
+   
     def test_rsvp_meetups(self):
         """ Allows users to confirm meetup attendance """
         meetup = {
@@ -178,33 +150,4 @@ class MeetupTest(SetUpTestClient):
         self.assertEqual(res.status_code, 201)
         res = self.client.post("/api/v1/meetups/50/rsvp", json=rsvp_two,
                                content_type='application/json')
-        self.assertEqual(res.status_code, 404)
-
-    def test_get_a_question(self):
-        """ Allows a user to fetch a specific question """
-        question = {
-                "user": 1,
-                "meetup": 1,
-                "title": "How do i install python in Ubuntu 18.04?",
-                "body": "I have been trying to install python in ubuntu\
-                                        18.04 with no success, someone help please."
-        }
-        question_two = {
-                "user": 1,
-                "meetup": 1,
-                "title": "How do i install python in Ubuntu 18.04?",
-                "body": "I have been trying to install python in ubuntu\
-                                18.04 with no success, someone help please."
-        }
-        res = self.client.post("/api/v1/questions", json=question,
-                               content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        res = self.client.post("/api/v1/questions", json=question_two,
-                               content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        res = self.client.get("/api/v1/questions/1")
-        self.assertEqual(res.status_code, 200)
-        res = self.client.get("/api/v1/questions/2")
-        self.assertEqual(res.status_code, 200)
-        res = self.client.get("/api/v1/questions/100")
         self.assertEqual(res.status_code, 404)
