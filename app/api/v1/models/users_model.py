f0fa45aee780a9ae02231bb3515a885c4bff7155
Felix Ouma <fatonoh@gmail.com>
2019-01-21 07:32:56 +0300

    [bug #163348045] Updated the users model file to pep8 standards

--- app/api/v1/models/users_model.py
+++ app/api/v1/models/users_model.py
@@ -18,8 +18,10 @@ class UsersModel:
 
     def add_user(self, user_request):
         """ register a user """
-        keys_expected = ['firstname', 'lastname', 'othername', 'username', 'email', 'isAdmin', 'password', 'cpassword', 'phoneNumber']
-        
+        keys_expected = ['firstname', 'lastname', 'othername', 'username',
+                         'email', 'isAdmin', 'password', 'cpassword',
+                         'phoneNumber']
+
         ret = helpers.is_valid_user_request(keys_expected, user_request)
 
         if ret[0] == 0:
@@ -40,7 +42,7 @@ class UsersModel:
         if ret[0] == 0:
             return ret[1], 409
 
-        validator = helpers.is_valid_email(user_request.get_json()['email'])  
+        validator = helpers.is_valid_email(user_request.get_json()['email'])
         if not validator[0]:
             return validator[1], 400
 
@@ -49,8 +51,8 @@ class UsersModel:
         if ret[0] == 0:
             return ret[1], 400
 
-        self.insert_db(user_request)     
-        return jsonify({'msg': 'user added successfully'}), 200
+        self.insert_db(user_request)
+        return jsonify({'msg': 'user added successfully', 'data': users}), 201
 
     def insert_db(self, user_request):
         new_user = {}
@@ -73,14 +75,14 @@ class UsersModel:
         global users
 
         if len(users) == 0:
-            return jsonify({'msg': 'no user was found'})    
+            return jsonify({'msg': 'no user was found'})
         return jsonify({"data": users}), 200
-    
+
     def token_required(self, f):
         @wraps(f)
         def decorated(*args, **kwargs):
             token = request.args.get('token') or request.get_json()['token']
-            #return jsonify({'msg': jwt.decode(token, 'Felix45')})
+ 
             if not token:
                 return jsonify({'msg': 'need to login to view this'}), 403
             try:
@@ -108,12 +110,13 @@ class UsersModel:
         username = request.get_json()['username']
         password = request.get_json()['password']
 
-        logged_in = [u for u in users if u['username'] == username and 
+        logged_in = [u for u in users if u['username'] == username and
                      check_password_hash(u['password'], password)]
 
         if len(logged_in) > 0:
             token = jwt.encode({"user": username,
-                                "exp": datetime.datetime.utcnow() + datetime.timedelta(30)
+                                "exp": datetime.datetime.utcnow() +
+                                datetime.timedelta(30)
                                 }, 'Felix45')
             return jsonify({'msg': 'logged in', "data": logged_in, "token":
                             token.decode('UTF-8')}), 200
