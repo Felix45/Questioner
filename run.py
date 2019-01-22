7c555debb8296b4f8db702bead5fe0b1eca3f53b
Felix Ouma <fatonoh@gmail.com>
2019-01-22 17:32:32 +0300

    [finishes #163348045] Finished working on fixing failing build

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 """ run the flask application """
+import os
 from app import create_app
 
-app = create_app()
+app = create_app(os.getenv('FLASK_ENV'))
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
