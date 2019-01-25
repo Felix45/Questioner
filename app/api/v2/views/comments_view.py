1a6942d8aeddd048b2d65343b303ee980e7481b3
Felix Ouma <fatonoh@gmail.com>
2019-01-25 06:23:34 +0300

    [feature #163429710] Add comments view for comments route

--- app/api/v2/views/comments_view.py
+++ app/api/v2/views/comments_view.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, Flask, jsonify, request
-
+from app.api.v2.models.meetups_model import UsersModel
 from app.api.v2.models.comments_model import CommentsModel
 
 commentV2 = Blueprint('comments_v2', __name__, url_prefix='/api/v2')
 
+
 comments_model = CommentsModel()
+users_model = UsersModel()
 
 
 @commentV2.route('/comments', methods=['POST'])
+@users_model.token_required
 def add_comment():
     return comments_model.add_comment(request)
