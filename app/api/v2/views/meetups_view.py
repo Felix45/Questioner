d396b98e9bec3d507a545eec6aebfbcb5b7e2094
Felix Ouma <fatonoh@gmail.com>
2019-01-25 06:17:24 +0300

    [feature #163429710] Added decorators to meetups and questions views

--- app/api/v2/views/meetups_view.py
+++ app/api/v2/views/meetups_view.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Flask, jsonify, request
 
 from app.api.v2.models.meetups_model import MeetUpsModel
 from app.api.v2.models.meetups_model import UsersModel
+from app.api.v2.models.rsvp_models import RsvpModel
 
 meetupV2 = Blueprint('meetup_v2', __name__, url_prefix='/api/v2')
 
@@ -15,6 +16,13 @@ def add_meetup():
 	return meetups_model.add_meetup(request)
 
 
+@meetupV2.route('/meetups/delete/<int:id>', methods=['DELETE'])
+@users_model.token_required
+def delete_meetup(id):
+	return meetups_model.delete_meetup(id)
+
+
+
 @meetupV2.route('/meetups/upcoming', methods=['GET'])
 def get_meetup():
 	return meetups_model.get_meetups()
@@ -23,10 +31,4 @@ def get_meetup():
 @meetupV2.route('/meetups/<int:meetup_id>', methods=['GET'])
 def get_a_meetup(meetup_id):
 	return meetups_model.get_a_meetup(meetup_id)
-
-
-@meetupV2.route("/meetups/<int:meetup_id>/rsvp", methods=['POST'])
-def rsvp_meetup(meetup_id):
-	return meetups_model.rsvp_meetup(meetup_id, request)
-
 
--- app/api/v2/views/questions_view.py
+++ app/api/v2/views/questions_view.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, Flask, jsonify, request
 
 from app.api.v2.models.questions_model import QuestionsModel
+from app.api.v2.models.questions_model import UsersModel
 
 questionV2 = Blueprint('question_v2', __name__, url_prefix='/api/v2')
 
 questions_model = QuestionsModel()
+users_model = UsersModel()
 
 
 @questionV2.route('/questions', methods=['POST'])
+@users_model.token_required
 def add_question():
 	return questions_model.add_a_question(request)
 
@@ -17,6 +20,7 @@ def get_question(question_id):
 
 
 @questionV2.route("/questions/<int:question_id>/<string:vote>", methods=['PATCH'])
+@users_model.token_required
 def vote(question_id, vote):
 	return questions_model.vote(question_id, vote)
 
