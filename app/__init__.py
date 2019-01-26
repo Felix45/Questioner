a5c1ef6831c96ff9787636374cd0e66e1ce022cd
Felix Ouma <fatonoh@gmail.com>
2019-01-26 23:35:56 +0300

    [bug #163303642] Add application blueprints to the app file in app directory

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from app.api.v1.views.user_view import userV1
 from app.api.v1.views.meetups_view import meetupV1
+from app.api.v1.views.questions_view import questionV1
 
 
 def create_app():
@@ -10,6 +11,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'Okish45@@'
     app.register_blueprint(userV1, url_prefix='/api/v1')
     app.register_blueprint(meetupV1, url_prefix='/api/v1')
+    app.register_blueprint(questionV1, url_prefix='/api/v1')
 
     @app.route("/")
     def function():
